3af6d613214e3dd3851b12d45707ede4ff01c261
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-02-27 20:08:43 +0900

    Title: 비교 연산, Time: 59ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 260227/비교 연산/comparison-operator.js
+++ 260227/비교 연산/comparison-operator.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 let [A, B] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-A>=B?console.log(1):console.log(0);
-A>B?console.log(1):console.log(0);
-B>=A?console.log(1):console.log(0);
-B>A?console.log(1):console.log(0);
-A===B?console.log(1):console.log(0);
-A!=B?console.log(1):console.log(0);
+console.log(a >= b ? 1 : 0);
+console.log(a > b ? 1 : 0);
+console.log(a <= b ? 1 : 0);
+console.log(a < b ? 1 : 0);
+console.log(a == b ? 1 : 0);
+console.log(a != b ? 1 : 0);
