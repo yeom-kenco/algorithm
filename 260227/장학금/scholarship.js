6d63fd757fc3a01974826457c6bf8513589c4b72
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-02-27 21:38:49 +0900

    Title: 장학금, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260227/장학금/scholarship.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+if(a<90||b<90){
+    console.log(0);
+}else if(b>=95){
+    console.log(100000);
+}else if(b>=90){
+    console.log(50000);
+}
