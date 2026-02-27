b951b244d95d29f48e67eb10d5b09f317412f946
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-02-27 19:39:43 +0900

    Title: 등급 매기기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260227/등급 매기기/ranking.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+if(n>=90){
+    console.log("A")
+}else if(n>=80){
+    console.log("B")
+}else if(n>=70){
+    console.log("C")
+}else if(n>=60){
+    console.log("D")
+}else{
+    console.log("F")
+}
