4d2793f66bf6205428d436b64cee7814c82feea3
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-02-27 20:34:47 +0900

    Title: 특정 조건 세 정수 비교, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260227/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 260227/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let [a, b, c] = fs.readFileSync(0).toString().split(" ").map(Number);
 
-resultA= (a<=b && a<=c)?1:0;
-resultB= (a==b==c)?1:0;
+resultA = (a<=b && a<=c)?1:0;
+resultB = (a==b && b==c)?1:0;
 console.log(resultA, resultB);
