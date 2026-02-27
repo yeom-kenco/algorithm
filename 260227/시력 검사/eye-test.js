fe571496983005258e1fca21585e8bc49aa9688f
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-02-27 20:30:27 +0900

    Title: 시력 검사, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260227/시력 검사/eye-test.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().split("\n").map(Number);
+
+if(a>=1.0 && b>=1.0){
+    console.log("High");
+}else if(a>=0.5 && b>=0.5){
+    console.log("Middle");
+}else{
+    console.log("Low")
+}
