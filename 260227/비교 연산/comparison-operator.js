269e678d256f41bc4c10f97c385816d267c320fe
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-02-27 20:09:00 +0900

    Title: 비교 연산, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 260227/비교 연산/comparison-operator.js
+++ 260227/비교 연산/comparison-operator.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let [A, B] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 console.log(a >= b ? 1 : 0);
 console.log(a > b ? 1 : 0);
