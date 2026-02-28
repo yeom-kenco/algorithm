17e2d6c1b68656906b012091af71a99e482b5c69
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-03-01 00:32:49 +0900

    Title: 중앙값 구하기, Time: 68ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 260301/중앙값 구하기/find-the-median.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let [A, B, C] = fs.readFileSync(0).toString().split(" ").map(Number);
+
+let middle;
+
+if (A > B) {
+    if (A < C) {
+        middle = A;
+    } else {
+        middle = C;
+    }
+} else {
+    if (B < C) {
+        middle = B;
+    } else {
+        middle = C;
+    }
+}
+console.log(middle);
