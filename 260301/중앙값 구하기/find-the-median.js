c0f50b8b36473af0a397559c39f76c2e9bee51eb
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-03-01 00:38:51 +0900

    Title: 중앙값 구하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/중앙값 구하기/find-the-median.js
+++ 260301/중앙값 구하기/find-the-median.js
@@ -6,13 +6,17 @@ let middle;
 if (A > B) {
     if (A < C) {
         middle = A;
+    }else if(C<B){
+        middle = B;
     } else {
         middle = C;
     }
 } else {
     if (B < C) {
         middle = B;
-    } else {
+    }else if(C<A){
+        middle = A;
+    }else {
         middle = C;
     }
 }
