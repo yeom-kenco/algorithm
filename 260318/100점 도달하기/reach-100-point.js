d9623c04b8ab766f1a1a822f0f2ef717ef10cf9f
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-03-18 13:58:57 +0900

    Title: 100점 도달하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260318/100점 도달하기/reach-100-point.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let N = Number(fs.readFileSync(0).toString());
+let result = "";
+
+for (let i=N; i<=100; i++){
+    if(i>=90){
+        result += "A" + " ";
+    }else if(i>=80){
+        result += "B" + " ";
+    }else if(i>=70){
+        result += "C" + " ";
+    }else if(i>=60){
+        result += "D" + " ";
+    }else{
+        result += "F" + " ";
+    }
+}
+console.log(result);
