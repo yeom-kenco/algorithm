3e8ddb8438ffa9479d327892f5ac31e6bcc7358b
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-03-23 20:13:03 +0900

    Title: 1까지 나누기, Time: 82ms, Memory: 10MB, Status: Passed - Codetree

--- 260323/1까지 나누기/divide-by-1.js
+++ 260323/1까지 나누기/divide-by-1.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
-let a = n;
 let cnt = 0;
 
-for(let i=1;i<=a;i++){
-    if(n<1){
+for (let i = 1; ; i++) {
+    n = Math.floor(n / i); 
+    cnt++;
+
+    if (n <= 1) {
         console.log(cnt);
         break;
     }
-    n = n/i;
-    cnt ++;
 }
