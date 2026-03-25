2a83768e849b9501367a043f24f632ceaadf50cb
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-03-25 16:41:06 +0900

    Title: 정사각형 두 개 출력, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260325/정사각형 두 개 출력/output-two-rectangles.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let str = "";
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
+console.log("");
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
