e0a5017af1dff42360458110b865bafa1f0af7e6
Kenco <75061562+yeom-kenco@users.noreply.github.com>
2026-04-08 20:14:53 +0900

    Title: 별표 출력하기 10, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/별표 출력하기 10/print-star-10.js
+++ 260408/별표 출력하기 10/print-star-10.js
@@ -1,18 +1,14 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
-let str = "";
 let cnt1 = 1;
 let cnt2 = n;
+
 for (let i = 1; i <= 2 * n; i++) {
-    if (i % 2 == 0) {
-        str = "";
-        str = "* ".repeat(cnt2);
-        cnt2--;
-        console.log(str);
-    } else {
-        str = "";
-        str = "* ".repeat(cnt1);
+    if (i % 2 !== 0) {
+        console.log("* ".repeat(cnt1));
         cnt1++;
-        console.log(str);
+    } else {
+        console.log("* ".repeat(cnt2));
+        cnt2--;
     }
 }
